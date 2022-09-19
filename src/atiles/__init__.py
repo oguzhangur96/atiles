a824d933b7a27fd93fd8856e87c08e2d5c8c060e
Oguzhan Gur <oguzhan_gur@outlook.com>
2022-09-19 11:27:57 +0300

    feat: to and from json

--- src/atiles/__init__.py
+++ src/atiles/__init__.py
@@ -1,7 +1,21 @@
 """Contains main class of the package, TileMap"""
+import json
+
+from atiles.models import TileMapSchema
+
 
 # TODO: implement and check functions
 # TODO: upload to test pypi
+# TODO: implement validation functions
+def map_from_json(path: str) -> None:
+    with open(path) as f:
+        data = json.load(f)
+        validated_data = TileMapSchema().load(data=data)
+        return TileMap(
+            terrains=validated_data["terrains"],
+            tiles=validated_data["tiles"],
+            metadata=validated_data["metadata"],
+        )
 
 
 class TileMap:
@@ -10,17 +24,21 @@ class TileMap:
         self.tiles = tiles
         self.metadata = metadata
 
-    # TODO: implement from_json
-    def from_json(self, path: str) -> None:
-        pass
-
     # TODO: implement visualize
     def visualize(self) -> None:
         pass
 
     # TODO: implement to_json
     def to_json(self, path: str) -> None:
-        pass
+        validated_data = TileMapSchema().load(
+            data={
+                "terrains": self.terrains,
+                "tiles": self.tiles,
+                "metadata": self.metadata,
+            }
+        )
+        with open(path, "w") as f:
+            json.dump(validated_data, f, indent=4, sort_keys=True)
 
     # TODO: implement to_image
     def to_image(self, path: str) -> None:
